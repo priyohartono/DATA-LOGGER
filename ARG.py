216e2e8b381fb431491a974a1ed62984eba5cc2d
priyohartono <yoyok.priyohartono@bmkg.go.idm>
2023-09-18 14:51:33 +0800

    htrhrth

--- ARG.py
+++ ARG.py
@@ -224,10 +224,9 @@ try:
             # Write the updated data to the CSV file
             add_to_lastvalue(filenamevalue, data)
 
-        if count_tips == 1 :
+        if tip_count == +1 :
             new_value = data
             append_to_csv(filenamevalue, new_value)
-
 
         # Data 1 menit
         if dt_utc.second == 0:

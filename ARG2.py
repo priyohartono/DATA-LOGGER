355e4761eeb68dfd4fbfeec284eedd4b6011e209
priyohartono <yoyok.priyohartono@bmkg.go.idm>
2023-12-11 04:08:10 +0000

    fdg

--- ARG2.py
+++ ARG2.py
@@ -212,9 +212,9 @@ try:
 
         # Draw the text and analog value on the image
         draw.text((0, 0), "   ARG REKAYASA SMD", font=font, fill=255)
-        draw.text((0, 4), " "+date_string+"", font=font, fill=255)
-        draw.text((0, 8),"   RR:"+RR+" BATT:"+volt+"", font=font, fill=255)
-        draw.text((0, 12), "   ARG REKAYASA SMD", font=font, fill=255)
+        draw.text((0, 12), " "+date_string+"", font=font, fill=255)
+        draw.text((0, 20),"   RR:"+RR+" BATT:"+volt+"", font=font, fill=255)
+
         # Display the image
         oled.image(image)
         oled.show()

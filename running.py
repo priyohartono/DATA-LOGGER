7600dd5a738c078d4f98c1f12b56a819944b17d9
priyohartono <yoyok.priyohartono@bmkg.go.idm>
2023-12-12 07:25:13 +0000

    hjl

--- running.py
+++ running.py
@@ -9,14 +9,14 @@ device = ssd1306(serial)
 
 # Running text message
 message = "Scrolling text on OLED: "
-font_width = 12  # Assuming font width is 8 pixels
+font_width = 24  # Assuming font width is 8 pixels
 
 try:
     while True:
         # Scroll the text to the left
-        for i in range(len(message)):
+        for i in range(len(message) * font_width + device.width):
             with canvas(device) as draw:
-                draw.text((-i, 8), message, fill=255)
+                draw.text((-i, 16), message, fill=255)
 
             # Pause for a short time
             time.sleep(0.05)
@@ -26,4 +26,4 @@ except KeyboardInterrupt:
 finally:
     # Clear the display on exit
     with canvas(device) as draw:
-        draw.text((0, 8), " ", fill=255)
+        draw.text((0, 8), fill=255)

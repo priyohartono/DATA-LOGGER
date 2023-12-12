1c09a20f767d801337c7cb1e7fe99991b1d4e824
priyohartono <yoyok.priyohartono@bmkg.go.idm>
2023-12-12 07:10:12 +0000

    asd

--- running.py
+++ running.py
@@ -1,28 +1,20 @@
 import time
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
-from luma.core.render import canvas
 
 # Set up the OLED display
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
 
 # Running text message
-message = "Scrolling text on OLED: "
+message = "Scrolling text on OLED: Hello, World! "
 
 try:
     while True:
-        # Clear the display
-        with canvas(device) as draw:
-            draw.text((0, 16), message, fill="white")
-
-        # Pause for a short time
-        time.sleep(0.5)
-
         # Scroll the text to the left
-        for i in range(len(message) * 8 + 1):
-            with canvas(device) as draw:
-                draw.text((-i, 16), message, fill="white")
+        for i in range(len(message) * 8 + device.width):
+            with device as draw:
+                draw.text((-i, 0), message, fill="white")
 
             # Pause for a short time
             time.sleep(0.05)
@@ -31,5 +23,5 @@ except KeyboardInterrupt:
     pass
 finally:
     # Clear the display on exit
-    with canvas(device) as draw:
-        draw.text((0, 16), " ", fill="black")
+    with device as draw:
+        draw.text((0, 0), " ", fill="black")

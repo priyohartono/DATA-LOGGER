fea788c4cf2920cea1a617d907a9b5423509db72
priyohartono <yoyok.priyohartono@bmkg.go.idm>
2023-12-12 01:14:26 +0000

    asf

--- running.py
+++ running.py
@@ -3,8 +3,7 @@ import board
 import busio
 import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.analog_in import AnalogIn, P0
 
 # Set up the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -13,8 +12,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
 # Set up the ADS1115 ADC
-ads = ADS.ADS1115(i2c)
-chan = AnalogIn(ads, ADS.P0)
+ads = AnalogIn(i2c, P0)
 
 # Create a blank image for drawing
 width = oled.width
@@ -24,26 +22,35 @@ draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 
 # Running text message
-message = "Running text with ADS1115: "
+message = "Scrolling text with ADS1115: "
 
 try:
     while True:
         # Read analog value from ADS1115
-        analog_value = chan.value
+        analog_value = ads.value
 
         # Clear the image
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-        # Draw the text and analog value on the image
-        draw.text((0, 0), message, font=font, fill=255)
-        draw.text((0, 16), f"Analog Value: {analog_value}", font=font, fill=255)
+        # Draw the scrolling text and analog value on the image
+        text_width, text_height = draw.textsize(message, font=font)
+        x = width
+        y = (height - text_height) // 2
+        draw.text((x, y), message, font=font, fill=255)
+
+        # Move the text to the left
+        x -= 1
+
+        # If the text has moved completely off the left side, reset its position
+        if x < -text_width:
+            x = width
 
         # Display the image
         oled.image(image)
         oled.show()
 
         # Pause for a short time
-        time.sleep(0.5)
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     pass
